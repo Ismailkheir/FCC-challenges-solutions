c1ca198f3361645fa6d28850f8cd7c95b5afcc0b
Ismail Kheir <ismailkheirbn@gmail.com>
2016-08-05 23:45:50 +0100

    Update symmetric_difference.js

--- symmetric_difference.js
+++ symmetric_difference.js
@@ -1,37 +1,36 @@
 //removes all the repetitions in one array
-	function noRep(arr){
-		var result=[];
-		for(var i=0;i<arr.length;i++){
-			if(result.indexOf(arr[i])===-1){
-				result.push(arr[i]);
-			}
+function noRep(arr){
+	var result=[];
+	for(var i=0;i<arr.length;i++){
+		if(result.indexOf(arr[i])===-1){
+			result.push(arr[i]);
 		}
-		return result;
 	}
+	return result;
+}
 
 //intersection of two arrays
-	function inter(arr1,arr2){
-		var result=[];
-		for(var i=0;i<arr1.length;i++){
-			for(var j=0;j<arr2.length;j++){
-				if(arr1[i]===arr2[j]){
-					result.push(arr1[i]);
-				}
+function inter(arr1,arr2){
+	var result=[];
+	for(var i=0;i<arr1.length;i++){
+		for(var j=0;j<arr2.length;j++){
+			if(arr1[i]===arr2[j]){
+				result.push(arr1[i]);
 			}
 		}
-
-		return noRep(result);
 	}
+	return noRep(result);
+}
 
 //sym of two arrays
 function symTwo(arr1,arr2){
-	var args=[];
-	args.push(arr1,arr2);
+	var twoArraysInOne=[];
+	twoArraysInOne.push(arr1,arr2);
 	var result=[];
 	//union of all the arrays with no repetition
-	var union=noRep(args.reduce(function(a,b){return a.concat(b);}));
+	var union=noRep(twoArraysInOne.reduce(function(a,b){return a.concat(b);}));
 	//intersection of all the arrays
-	var inters=args.reduce(function(a,b){
+	var inters=twoArraysInOne.reduce(function(a,b){
 			return inter(a,b);
 	});
 	result=union.filter(function(elu){return inters.indexOf(elu)===-1;});
